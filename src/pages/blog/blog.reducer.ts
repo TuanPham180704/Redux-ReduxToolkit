06d442caf55a1715961f1061e4e1db7b5ca7c6ae
Tuandev18 <phamtuan180704@gmail.com>
2025-08-09 19:38:17 +0700

    DeletePost

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -10,13 +10,21 @@ const initialState: BlogState = {
   postList: initialPostList
 }
 
-export const addPost =createAction<Post>('blog/addPost')
-
+export const addPost = createAction<Post>('blog/addPost')
+export const deletePost = createAction<string>('blog/deletePost')
 const blogReducer = createReducer(initialState, (builder) => {
-  builder.addCase(addPost,(state,action)=>{
-    const post = action.payload
-    state.postList.push(post)
-  })
+  builder
+    .addCase(addPost, (state, action) => {
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(deletePost, (state, action) => {
+      const postId = action.payload
+      const foundIDdelete = state.postList.findIndex((post) => post.id === postId)
+      if (foundIDdelete !== -1) {
+        state.postList.splice(foundIDdelete, 1)
+      }
+    })
 })
 
 export default blogReducer
--- src/pages/blog/components/PostItem/PostItem.tsx
+++ src/pages/blog/components/PostItem/PostItem.tsx
@@ -2,10 +2,11 @@ import { Post } from "types/blog.type"
 
 interface PostItemType {
   post : Post
+  handleDelete : (postId: string) => void
 }
 
 
-export default function PostItem({post} : PostItemType) {
+export default function PostItem({post,handleDelete} : PostItemType) {
   return (
      <div className='flex flex-col items-center overflow-hidden rounded-lg border md:flex-row'>
             <div className='group relative block h-48 w-full shrink-0 self-start overflow-hidden bg-gray-100 md:h-full md:w-32 lg:w-48'>
@@ -27,6 +28,7 @@ export default function PostItem({post} : PostItemType) {
               <div>
                 <div className='inline-flex rounded-md shadow-sm' role='group'>
                   <button
+                    
                     type='button'
                     className='rounded-l-lg border border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
                   >
@@ -34,6 +36,7 @@ export default function PostItem({post} : PostItemType) {
                   </button>
                   <button
                     type='button'
+                    onClick={()=>handleDelete(post.id)}
                     className='rounded-r-lg border-t border-b border-r border-gray-200 bg-white py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700'
                   >
                     Delete
